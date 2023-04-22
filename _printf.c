10c567d19e1b4cbf72de1a2506ce9ba147638b42
soumeyahaloui <soumeya.haloui@gmail.com>
2023-04-22 14:11:31 -0700

    updated

--- _printf.c
+++ _printf.c
@@ -26,7 +26,6 @@ int _printf(const char *format, ...)
 			buffer[buff_ind++] = format[i];
 			if (buff_ind == BUFF_SIZE)
 				print_buffer(buffer, &buff_ind);
-			/* write(1, &format[i], 1);*/
 			printed_chars++;
 		}
 		else
@@ -53,7 +52,7 @@ int _printf(const char *format, ...)
 }
 
 /**
- * print_buffer - Prints the contents of the buffer if it exist
+ * print_buffer - Prints the contents of the buffer if it exists
  * @buffer: Array of chars
  * @buff_ind: Index at which to add next char, represents the length.
  */
