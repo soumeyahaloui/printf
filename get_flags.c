03258d305a8502444b511395335317033f517001
amirakame <amirakamel343@gmail.com>
2023-04-22 19:06:06 -0700

    get_flags.c

--- get_flags.c
+++ get_flags.c
@@ -1,12 +1,12 @@
 #include "main.h"
 
 /**
- * get_size_flags - Calculates effective flags for get_size function
- * @format: Formatted string to print's arguments
+ * get_flags - Calculates active flags
+ * @format: Formatted string in which to print the arguments
  * @i: take a parameter.
  * Return: Flags:
  */
-int get_size_flags(const char *format, int *i)
+int get_flags(const char *format, int *i)
 {
 	/* - + 0 # ' ' */
 	/* 1 2 4 8  16 */
@@ -27,6 +27,8 @@ int get_size_flags(const char *format, int *i)
 		if (FLAGS_CH[j] == 0)
 			break;
 	}
+
 	*i = curr_i - 1;
+
 	return (flags);
 }
