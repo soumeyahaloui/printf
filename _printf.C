03156cbd0e672862ae8affbccdc1ed2e2453ceb6
soumeyahaloui <soumeya.haloui@gmail.com>
2023-04-24 16:48:26 -0700

    updated

--- _printf.C
+++ _printf.C
@@ -1,58 +1,65 @@
 #include "main.h"
 
+#define BUFF_SIZE 1024
+
 /**
- * _printf - Custom implementation of printf function
- * @format: The format string
- *
- * This function implements a custom version of the printf function, which
- * prints formatted output based on the provided format string. The format
- * string can contain format specifiers that are replaced by corresponding
- * values passed as additional arguments.
- *
- * Return: Number of bytes printed
+ * print_buffer - Prints the contents of the buffer if it exists
+ * @buffer: Array of chars
+ * @buff_ind: Index at which to add next char, represents the length.
+ */
+void print_buffer(char buffer[], int *buff_ind)
+{
+	if (*buff_ind > 0)
+		write(1, &buffer[0], *buff_ind);
+
+	*buff_ind = 0;
+}
+
+/**
+ * _printf - Printf function
+ * @format: format string
+ * Return: Number of characters printed.
  */
 int _printf(const char *format, ...)
 {
-	int sum = 0;
-	va_list ap;
-	char *p, *start;
-	params_t params = PARAMS_INIT;
+	int i, printed = 0, printed_chars = 0;
+	int flags, width, precision, size, buff_ind = 0;
+	va_list list;
+	char buffer[BUFF_SIZE];
 
-	va_start(ap, format);
-
-	if (!format || (format[0] == '%' && !format[1]))
+	if (format == NULL)
 		return (-1);
 
-	if (format[0] == '%' && format[1] == ' ' && !format[2])
-		return (-1);
+	va_start(list, format);
 
-	for (p = (char *)format; *p; p++)
+	for (i = 0; format && format[i] != '\0'; i++)
 	{
-		if (!finit_params(&params, ap))
+		if (format[i] != '%')
 		{
-			if (*p != '%')
-			{
-				sum += _putchar(*p);
-				continue;
-			}
-			start = p;
-			p++;
-			while (get_flag(p, &params)) /* while char at p is flag char */
-			{
-				p++; /* next char */
-			}
-			p = get_width(p, &params, ap);
-			p = get_precision(p, &params, ap);
-			if (get_modifier(p, &params))
-				p++;
-			if (!get_specifier(p))
-				sum += print_from_to(start, p, params.l_modifier || params.h_modifier ? p - 1 : 0);
-			else
-				sum += get_print_func(p, ap, &params);
+			buffer[buff_ind++] = format[i];
+			if (buff_ind == BUFF_SIZE)
+				print_buffer(buffer, &buff_ind);
+			printed_chars++;
+		}
+		else
+		{
+			print_buffer(buffer, &buff_ind);
+			flags = get_flags(format, &i);
+			width = get_width(format, &i, list);
+			precision = get_precision(format, &i, list);
+			size = get_size(format, &i);
+			++i;
+			printed = handle_print(format, &i, list, buffer,
+					flags, width, precision, size);
+			if (printed == -1)
+				return (-1);
+			printed_chars += printed;
 		}
 	}
 
-	_putchar(BUF_FLUSH);
-	va_end(ap);
-	return (sum);
+	print_buffer(buffer, &buff_ind);
+
+	va_end(list);
+
+	return (printed_chars);
 }
